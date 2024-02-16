949205decd2b47639e8b310ba33debc198d65447
yuchan <rladbcks56@naver.com>
2024-02-17 06:12:59 +0900

    audioManager

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -39,30 +39,19 @@ public class AudioManager : MonoBehaviour
         soundClip.Add("EndScream", clipList[11]);
     }
 
-<<<<<<< Updated upstream
-    public AudioSource audioSource;
-    public AudioSource SFXSource;
-    public List<AudioClip> clipList;
-    public Dictionary<string, AudioClip> soundClip = new Dictionary<string, AudioClip>();
 
-=======
-    
->>>>>>> Stashed changes
     public void AudioPlay(string audioName)
     {
-        audioSource.clip = soundClip[audioName];
         audioSource.PlayOneShot(soundClip[audioName]);
     }
 
     public void SFXPlay(string SFXName)
     {
-        SFXSource.clip = soundClip[SFXName];
         SFXSource.PlayOneShot(soundClip[SFXName]);
     }
 
     public void ChangeVolume(float value)
     {
-        audioSource.volume = value * 0.5f;
-        SFXSource.volume = value;
+        audioSource.volume = value;
     }
 }
