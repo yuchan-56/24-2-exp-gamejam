7fa448c74f8d748a87b7b30f82b1467ea999ad08
Whiteant1 <chamingy1@gmail.com>
2024-02-16 23:52:55 +0900

    3번방 점점 밝아짐 (안합침)

--- Assets/Script/Darkness.cs
+++ Assets/Script/Darkness.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Darkness : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float fadeDuration = 60f;
+    public float exponentialFactor = 3f;
+
+    private Image myImage;
+    private Color startColor;
+
     void Start()
     {
-        
+        myImage = GetComponent<Image>();
+
+        // 초기 색상 저장
+        startColor = myImage.color;
+
+        // 코루틴 시작
+        StartCoroutine(FadeOut());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeOut()
     {
-        
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            // 투명도를 시간에 따라 조절
+            float alpha = Mathf.Pow(elapsedTime / fadeDuration, exponentialFactor);
+
+            // 현재 색상을 가져와서 투명도를 조절한 후 적용
+            Color newColor = startColor;
+            newColor.a = 1 - alpha;
+            myImage.color = newColor;
+
+            // 시간 업데이트
+            elapsedTime += Time.deltaTime;
+
+            // 매 프레임마다 잠시 기다림
+            yield return null;
+        }
+
+        // 투명도가 0이 되면 이미지 비활성화 또는 다른 작업 수행
+        gameObject.SetActive(false);
     }
 }
