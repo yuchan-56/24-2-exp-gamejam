cf411a17a51bb3e677bd9845fa2fe2f56ddb0c66
yuchan <rladbcks56@naver.com>
2024-02-17 01:53:12 +0900

    darkness 찐완성

--- Assets/Script/Darkness.cs
+++ Assets/Script/Darkness.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,25 +10,15 @@ public class Darkness : MonoBehaviour
     public Image background;
     public Button skull;
 
-    void Start()
+    private void Update()
     {
-        StartCoroutine(FadeIn());
-    }
-
-    IEnumerator FadeIn()
-    {
-        while (Timer.Instance.time < 60f)
-        {
-            background.color = new Color(0, 0, 0, 
+        background.color = new Color(0, 0, 0,
                 Mathf.Lerp(1, 0, Timer.Instance.time / 60f));
 
-            if (Timer.Instance.time > 15f)
-            {
-                skull.interactable = true;
-            }
-
-            yield return null;
+        if (Timer.Instance.time > 15f)
+        {
+            skull.interactable = true;
         }
-        
     }
+
 }
