6a7055321dfd52fe669facc78d517bc173457c58
yuchan <rladbcks56@naver.com>
2024-02-17 06:28:10 +0900

    mainStage 이것저것

--- Assets/Script/MoonScale.cs
+++ Assets/Script/MoonScale.cs
@@ -8,7 +8,7 @@ public class MoonScale : MonoBehaviour
 
     private void Update()
     {
-        newScale = Mathf.Lerp(1, 10, Timer.Instance.time / 60f);
+        newScale = Mathf.Lerp(1, 4, Timer.Instance.time / 60f);
         transform.localScale = new Vector3(newScale, newScale, 0f);
     }
 }
