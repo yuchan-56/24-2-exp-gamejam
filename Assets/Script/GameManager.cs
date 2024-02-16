c27afee44e821767bdc80587689d13990ec87d72
yuchan <rladbcks56@naver.com>
2024-02-16 21:24:23 +0900

    새마음 새출발

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,29 +9,6 @@ public class GameManager : MonoBehaviour
     public int currentdirection;
     public GameObject[] direction = new GameObject[4];
 
-    public void Awake()
-    {
-        currentdirection = 0;
-    }
-    public void TurnLeft()
-    {
-        currentdirection--;
-        if (currentdirection <= -1)
-        {
-            currentdirection = 3;
-        }
-        Move(currentdirection);
-    }
-
-    public void TurnRight()
-    {
-        currentdirection++;
-        if (currentdirection >= 4)
-        {
-            currentdirection = 0;
-        }
-        Move(currentdirection);
-    }
     public void Move(int targetdirection)
     {
         for(int i = 0; i < direction.Length; i++)
