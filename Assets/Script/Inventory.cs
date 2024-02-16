a83fc8d9c58d68a95721aa3474222d7779fa255d
yuchan <rladbcks56@naver.com>
2024-02-16 22:26:32 +0900

    인벤토리 줌

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public enum Item
-    {
-        Key,
-        Note,
-        TornNote1,
-        TornNote2
-    }
 
-    public GameObject parent;
+    public GameObject inventoryParent, zoomParent;
     public GameObject[] prefab = new GameObject[4];
-    public List<Item> inventory = new List<Item>();
+    public List<int> inventory = new List<int>();
 
     public void GetItem(int item)
     {
-        Instantiate(prefab[item], parent.transform.GetChild(inventory.Count));
-        inventory.Add((Item)item);
+        Instantiate(prefab[item], inventoryParent.transform.GetChild(inventory.Count));
+        inventory.Add(item);
     }
+
+    public void ZoomItem(int item)
+    {
+        if (item >= inventory.Count) return;
+        zoomParent.transform.GetChild(inventory[item]).gameObject.SetActive(true);
+    }
+    
 }
