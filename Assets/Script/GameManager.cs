681c7a6efefdf63ce823b0e024483fdadc0a3d79
Whiteant1 <chamingy1@gmail.com>
2024-02-16 17:46:32 +0900

    인벤토리 UI

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Dictionary<Item, bool> hasItem;
+
+    public void Awake()
+    {
+        hasItem = new Dictionary<Item, bool>();
+    }
+
     public void Move(GameObject before, GameObject after)
     {
         before.SetActive(false);
         after.SetActive(true);
+    }
+
+    public void GetItem(Item item)
+    {
+        hasItem.Add(item, true);
     }
 }
---
+++ Assets/Script/Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public Image[] inventory = new Image[4];
+    public void UpdateInventory(Dictionary<Item,bool> hasItem)
+    {
+        
+    }
+}
