3741710f88842cd4fcb5ce512e92777a74bc7904
yuchan <rladbcks56@naver.com>
2024-02-18 22:02:27 +0900

    클리어화면 수정

--- Assets/Script/Clear.cs
+++ Assets/Script/Clear.cs
@@ -17,22 +17,22 @@ public class Clear : MonoBehaviour
         float newY;
         while (time < 18f)
         {
-            newY = Mathf.Lerp(11f, 5f, time / 18f);
-            gameObject.transform.position = new Vector3(0, newY, 0);
+            newY = Mathf.Lerp(3000f, 1600f, time / 18f);
+            gameObject.transform.localPosition = new Vector3(0, newY, 0);
             time += Time.deltaTime;
             yield return null;
         }
 
         time = 0;
         float rotZ;
-        while (time < 0.75f)
+        while (time < 2f)
         {
-            rotZ = Mathf.Lerp(0f, 180f, time / 0.75f);
+            rotZ = Mathf.Lerp(0f, 180f, time / 2f);
             gameObject.transform.rotation = Quaternion.Euler(0, 0, rotZ);
             time += Time.deltaTime;
             yield return null;
         }
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(6f);
         Application.Quit();
     }
 }
