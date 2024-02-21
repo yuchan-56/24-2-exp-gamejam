28e207564e58b8e8354450383004a63b021fe435
yuchan <rladbcks56@naver.com>
2024-02-21 22:48:10 +0900

    클리어씬 타이밍 수정

--- Assets/Script/Clear.cs
+++ Assets/Script/Clear.cs
@@ -15,9 +15,9 @@ public class Clear : MonoBehaviour
     {
         float time = 0;
         float newY;
-        while (time < 18f)
+        while (time < 18.5f)
         {
-            newY = Mathf.Lerp(3000f, 1600f, time / 18f);
+            newY = Mathf.Lerp(3000f, 1600f, time / 18.5f);
             gameObject.transform.localPosition = new Vector3(0, newY, 0);
             time += Time.deltaTime;
             yield return null;
@@ -25,14 +25,14 @@ public class Clear : MonoBehaviour
 
         time = 0;
         float rotZ;
-        while (time < 2f)
+        while (time < 0.7f)
         {
-            rotZ = Mathf.Lerp(0f, 180f, time / 2f);
+            rotZ = Mathf.Lerp(0f, 180f, time / 0.7f);
             gameObject.transform.rotation = Quaternion.Euler(0, 0, rotZ);
             time += Time.deltaTime;
             yield return null;
         }
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(2f);
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
